4abf88e06ae99db2884d4a8ddd4ecca6688c6a88
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-07-11 01:52:14 +0000

    user registration

--- express_server.js
+++ express_server.js
@@ -25,6 +25,16 @@ function generateRandomString() {
   return result;
 }  
 
+function urlsForUser(id) {
+  let result = {};
+  for (let i in urlDatabase) {
+    if(urlDatabase[i].userID === id) {
+      result[i] = urlDatabase[i];
+    }
+  }
+  return result;
+}
+
 const users = { 
   "userRandomID": {
     id: "userRandomID", 
@@ -38,9 +48,14 @@ const users = {
   }
 }
 
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
 app.set('view engine', 'ejs')
@@ -59,33 +74,43 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.render('urls_index',{urls: urlDatabase, user: users[req.cookies["user_id"]]});
+  const id = req.cookies["user_id"];
+  const displayURLS = urlsForUser(id);
+  res.render('urls_index',{urls: displayURLS, user: users[req.cookies["user_id"]]});
 });
 
 app.get("/urls/new", (req, res) => {
+  console.log(req.cookies['user_id'])
+  if (!req.cookies['user_id']) {
+    res.redirect('/urls')
+  }
   res.render("urls_new", {user: users[req.cookies["user_id"]]});
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
+  const templateVars = { shortURL: req.params.id, URL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
   res.render('urls_show.ejs', templateVars);
 });
 
 app.post("/urls", (req, res) => {
   const shorturl = generateRandomString();
-  urlDatabase[shorturl] =  req.body.longURL;
+  urlDatabase[shorturl] =  {longURL:req.body.longURL, userID: req.cookies["user_id"]}
   res.redirect('/urls');///////
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
-  res.redirect(longURL);
+  const longURL = urlDatabase[req.params.shortURL].longURL
+  res.redirect(`${longURL}`);
 });
 
 app.post('/urls/:shortURL/delete', (req, res) => {
-
-  delete urlDatabase[req.params.shortURL]
-  res.redirect('/urls');
+  const id = req.cookies['user_id']
+  if (urlDatabase[req.params.shortURL].userID === id) {
+    delete urlDatabase[req.params.shortURL]
+    res.redirect('/urls');
+  }
+  res.status(400)
+  res.send('you have no access to delete the url')
 })
 
 app.post('/urls/:shortURL', (req, res) => {
@@ -122,7 +147,8 @@ app.get('/login', (req, res) => {
 app.post('/login', (req, res) => {
   const userEmail = req.body.email
   const id = userLookup(userEmail).id
-  if (users[id].password === req.body.password) {
+  
+  if (users[id] && users[id].password === req.body.password) {
     res.cookie('user_id', id);
     res.redirect('/urls')
   }
